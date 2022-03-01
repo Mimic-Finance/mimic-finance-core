f85bb5afe26544c85ebea379e5ba38cf3e062b90
waranat suttikarn <waranat.cpe@gmail.com>
2022-03-02 03:58:51 +0700

    feat: add config.json

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,8 +1,9 @@
+const config = require("../config.json");
 const DappToken = artifacts.require("DappToken");
 const DaiToken = artifacts.require("Daitoken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = async function(deployer, network, accounts) {
+module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(DaiToken);
   const daiToken = await DaiToken.deployed();
 
@@ -13,7 +14,8 @@ module.exports = async function(deployer, network, accounts) {
   const tokenFarm = await TokenFarm.deployed();
 
   await dappToken.transfer(tokenFarm.address, "1000000000000000000000000");
-  await daiToken.transfer(accounts[1],
+  await daiToken.transfer(
+    config.mode === "development" ? accounts[1] : config.testerAddress,
     "100000000000000000000"
   );
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,18 @@
+const config = require("./config.json");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const mnemonic =
-  "6e8b93d37dbd7f83ea981f157e39c1461e81d68110e44b9a972173bc5494d691";
-const liveNetwork = "https://devnet.kmutt.me";
 
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*",
+      host: config.ganacheHost,
+      port: config.ganachePort,
+      network_id: config.ganacheNetworkId,
     },
 
     live: {
-      provider: () => new HDWalletProvider(mnemonic, liveNetwork),
-      network_id: "*",
+      provider: () =>
+        new HDWalletProvider(config.testerPrivateKey, config.liveHost),
+      network_id: config.liveNetworkId,
     },
   },
 
