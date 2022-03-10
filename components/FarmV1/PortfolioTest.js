0ede3a982ff02ab408cc8468df7a555d69f404aa
waranat suttikarn <waranat.cpe@gmail.com>
2022-03-11 00:42:56 +0700

    feat: claim reward button

--- components/FarmV1/PortfolioTest.js
+++ components/FarmV1/PortfolioTest.js
@@ -1,7 +1,13 @@
-import { Box, Text, Grid, GridItem } from "@chakra-ui/react";
-import Web3 from "web3";
+import { Box, Text, Grid, GridItem, Button } from "@chakra-ui/react";
+import useAppSelector from "../../hooks/useAppSelector";
 
 const Portfolio = ({ balance, reward, total }) => {
+  const { farmToken } = useAppSelector((state) => state.auth);
+
+  const claimReward = async () => {
+    await farmToken.methods.issueTokens().call();
+  };
+
   return (
     <Box mt={5}>
       <Text fontSize="xl">
@@ -20,6 +26,17 @@ const Portfolio = ({ balance, reward, total }) => {
             <Text mt={2} fontSize="m">
               $ {reward}
             </Text>
+            <Box pt={3}>
+              <Button
+                onClick={() => {
+                  claimReward();
+                }}
+                size="xs"
+                colorScheme="purple"
+              >
+                Claim
+              </Button>
+            </Box>
           </GridItem>
           <GridItem colSpan={3}>
             <Text fontSize="l">Totals</Text>
