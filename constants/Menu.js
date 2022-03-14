787503f764aacbfb94e3f33b9def4f1bac3b86a6
waranat suttikarn <waranat.cpe@gmail.com>
2022-03-15 05:22:03 +0700

    fix: NAV_ITEM to constant menu

---
+++ constants/Menu.js
@@ -0,0 +1,16 @@
+const MENU = [
+  {
+    label: "Swap",
+    href: "/swap",
+  },
+  {
+    label: "Vault",
+    href: "/vault",
+  },
+  {
+    label: "Faucet",
+    href: "/faucet",
+  },
+];
+
+export default MENU;
