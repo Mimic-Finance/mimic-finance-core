47ed55d2e658e311f508937062fb419b2aaf7235
aom wara <waranat.cpe@gmail.com>
2022-03-18 16:12:21 +0700

    fix: swal position to bottom-end

--- components/utils/Toast/Toast.js
+++ components/utils/Toast/Toast.js
@@ -2,7 +2,7 @@ import Swal from "sweetalert2";
 
 const Toast = Swal.mixin({
   toast: true,
-  position: "top-end",
+  position: "bottom-end",
   showConfirmButton: false,
   timer: 1500,
   timerProgressBar: true,
