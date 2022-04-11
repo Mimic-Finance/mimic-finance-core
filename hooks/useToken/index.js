5e3a6e1d5df24f0a94b050c43b72139304f8424c
aom wara <waranat.cpe@gmail.com>
2022-04-12 01:23:03 +0700

    feat: export usetoken

--- hooks/useToken/index.js
+++ hooks/useToken/index.js
@@ -1,5 +1,15 @@
 import useUSDC from "./useUSDC";
 import useBUSD from "./useBUSD";
+import useDAI from "./useDAI";
+import useUSDT from "./useUSDT";
+import useJUSD from "./useJUSD";
+import useCJUSD from "./usecJUSD";
+import useMIMIC from "./useMIM";
 
 export { useUSDC };
 export { useBUSD };
+export { useUSDT };
+export { useDAI };
+export { useMIMIC };
+export { useJUSD };
+export { useCJUSD };
