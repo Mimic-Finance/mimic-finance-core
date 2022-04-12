95521fac807488c8f86b072cd856cfc8a408f09d
aom wara <waranat.cpe@gmail.com>
2022-04-13 01:55:10 +0700

    fix: folder ignore upper case

---
+++ hooks/useToken/useCJUSD.js
@@ -0,0 +1,32 @@
+import useContract from "../useContract";
+import CJUSD_ABI from "../../abis/cJUSD.json";
+
+import useAppSelector from "hooks/useAppSelector";
+
+import { useCallback, useEffect, useState } from "react";
+
+const useCJUSD = () => {
+  const { account } = useAppSelector((state) => state.account);
+
+  const abi = CJUSD_ABI.abi;
+  const contract = useContract(abi, CJUSD_ABI.networks[1].address);
+  const [balance, setBalance] = useState(false);
+
+  const methods = contract.methods;
+  const address = contract._address;
+
+  const getBalance = useCallback(async () => {
+    const _balance = await contract.methods.balanceOf(account).call();
+    setBalance(_balance);
+  }, [account, contract.methods]);
+
+  useEffect(() => {
+    if (account) {
+      getBalance();
+    }
+  }, [getBalance, account]);
+
+  return { contract, methods, address, balance };
+};
+
+export default useCJUSD;
