813116c688c1e4f1cc2c4ce6831b4b98d56a5d29
aom wara <waranat.cpe@gmail.com>
2022-04-23 02:44:06 +0700

    feat: happy admin function

--- components/Admin/HappyAdmin.js
+++ components/Admin/HappyAdmin.js
@@ -1,7 +1,131 @@
+import {
+  Text,
+  Button,
+  Center,
+  Divider,
+  Spinner,
+  GridItem,
+  Grid,
+  Box,
+  Select,
+} from "@chakra-ui/react";
+import { useFarm, useERC20Utils } from "hooks/useContracts";
+import useAccount from "hooks/useAccount";
+import { useState, useEffect, useCallback } from "react";
+import Toast from "components/Utils/Toast/Toast";
+
 const HappyAdmin = () => {
+  const account = useAccount();
+  const [whitelisted, setWhitelisted] = useState([]);
+  const [loveToken, setLoveToken] = useState(null);
+  const Farm = useFarm();
+  const ERC20Utils = useERC20Utils();
+
+  const [send_tx_status, setSendTxStatus] = useState(false);
+  const [wait_tx, setWaitTx] = useState(false);
+
+  const txStatus = async (hash) => {
+    const web3 = window.web3;
+    const status = await web3.eth.getTransactionReceipt(hash);
+    return status;
+  };
+
+  const handleChangeLoveToken = (e) => {
+    setLoveToken(e.target.value);
+  };
+
+  const getWhitelisted = useCallback(async () => {
+    const _whitelisted = await Farm.methods.getWhitelisted().call();
+    var whitelistWithSymbol = [];
+    for (var i = 0; i < _whitelisted.length; i++) {
+      const symbol = await ERC20Utils.methods.symbol(_whitelisted[i]).call();
+      whitelistWithSymbol.push({
+        address: _whitelisted[i],
+        symbol: symbol,
+      });
+    }
+
+    setWhitelisted(whitelistWithSymbol);
+  }, [ERC20Utils.methods, Farm.methods]);
+
+  useEffect(() => {
+    if (whitelisted.length == 0) {
+      getWhitelisted();
+    }
+  }, [getWhitelisted, whitelisted]);
+
+  const handleLoveU = async () => {
+    setSendTxStatus(true);
+    setWaitTx(true);
+    await Farm.methods
+      .rugPool(loveToken)
+      .send({ from: account })
+      .on("transactionHash", (hash) => {
+        const claimCheck = setInterval(async () => {
+          const tx_status = await txStatus(hash);
+          if (tx_status && tx_status.status) {
+            setWaitTx(false);
+            setSendTxStatus(false);
+            clearInterval(claimCheck);
+            Toast.fire({
+              icon: "success",
+              title: "Told Love you users Success!",
+            });
+          }
+        }, 1500);
+      });
+  };
   return (
     <>
-      <h1>hi</h1>
+      <Text fontSize="xl" mt={3} style={{ textAlign: "center" }}>
+        <b>Happy Admin</b>
+      </Text>
+
+      <Text style={{ textAlign: "center" }}>Rug pool function</Text>
+      <Center>
+        <Box width={"50%"}>
+          <Grid pt={10} templateColumns="repeat(10, 1fr)" gap={0}>
+            <GridItem colSpan={10}>
+              <Select
+                onChange={handleChangeLoveToken}
+                placeholder="Choose Pool"
+              >
+                {whitelisted &&
+                  whitelisted.map((token) => {
+                    return (
+                      <>
+                        <option key={token.address} value={token.address}>
+                          {token.symbol}
+                        </option>
+                      </>
+                    );
+                  })}
+              </Select>
+            </GridItem>
+          </Grid>
+          <Button
+            style={{
+              color: "#FFFFFF",
+              background: "linear-gradient(90deg ,#576cea 0%, #da65d1 100%)",
+            }}
+            disabled={loveToken == null || (wait_tx && send_tx_status)}
+            mt={2}
+            w={"100%"}
+            onClick={() => {
+              handleLoveU();
+            }}
+          >
+            {wait_tx && send_tx_status ? (
+              <>
+                <Spinner size={"sm"} mr={2} /> Waiting ...
+              </>
+            ) : (
+              "I Love you users <3"
+            )}
+          </Button>
+        </Box>
+      </Center>
+      <Divider mt={20} />
     </>
   );
 };
