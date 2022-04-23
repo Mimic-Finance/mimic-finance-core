233280811cff092971b9d843ec6e42d6a121f9d0
aom wara <waranat.cpe@gmail.com>
2022-04-24 04:38:53 +0700

    refactor: remove contracts slice and reset portfolio page

--- store/rootReducer.js
+++ store/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 
-import contractsReducer from "../slices/contracts";
 import accountReducer from "../slices/account";
 
 const rootReducer = combineReducers({
   account: accountReducer,
-  contracts: contractsReducer,
 });
 
 export default rootReducer;
