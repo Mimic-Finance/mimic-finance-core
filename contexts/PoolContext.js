614bc11ae3263bae118a9cd5438b72d928dc2a7b
aom wara <waranat.cpe@gmail.com>
2022-04-25 23:02:12 +0700

    feat: display loading spinner when verifying pool

--- contexts/PoolContext.js
+++ contexts/PoolContext.js
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState, useCallback } from "react";
 import { useWhitelisted } from "hooks/useFunctions";
-import { Text, Center, Box } from "@chakra-ui/react";
+import { Text, Center, Box, Spinner } from "@chakra-ui/react";
 import { WarningIcon } from "@chakra-ui/icons";
 
 export const PoolContext = createContext(false);
@@ -9,12 +9,14 @@ export const PoolContextProvider = ({ children, address }) => {
   const getWhitelisted = useWhitelisted();
   const [whitelist, setWhitelisted] = useState([]);
   const [accessible, setAccessible] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const checkOpenPool = useCallback(async () => {
     const open = whitelist.filter((whitelist) => address === whitelist.address);
     if (open.length > 0) {
       setAccessible(true);
     }
+    setLoading(false);
   }, [whitelist, address]);
 
   useEffect(() => {
@@ -27,8 +29,21 @@ export const PoolContextProvider = ({ children, address }) => {
     }
   }, [checkOpenPool, whitelist.length]);
 
-  if (accessible) {
+  if (accessible && !loading) {
     return <PoolContext.Provider>{children}</PoolContext.Provider>;
+  } else if (loading) {
+    return (
+      <PoolContext.Provider>
+        <Center>
+          <Spinner size="xl" />
+        </Center>
+        <Center>
+          <Text fontSize="2xl" mt={3}>
+            Verifing Open Pool
+          </Text>
+        </Center>
+      </PoolContext.Provider>
+    );
   } else {
     return (
       <PoolContext.Provider>
