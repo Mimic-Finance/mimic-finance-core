3833edfb63ec9889c6044ee9b6805e01fc814f70
aom wara <waranat.cpe@gmail.com>
2022-05-02 20:50:53 +0700

    fix: Visibility for constructor [token contract]

--- contracts/Token/DAI.sol
+++ contracts/Token/DAI.sol
@@ -1,7 +1,8 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DAI is ERC20 {
-    constructor() public ERC20("DAI Coin", "DAI") {}
+    constructor() ERC20("DAI Coin", "DAI") {}
 }
--- contracts/Token/USDC.sol
+++ contracts/Token/USDC.sol
@@ -1,7 +1,8 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract USDC is ERC20 {
-    constructor() public ERC20("USDC Coin", "USDC") {}
+    constructor() ERC20("USDC Coin", "USDC") {}
 }
